719c3aed4dc2ad2cc51f2c7bc05d333566b09075
MDJAmin <mohammadamindf@gmail.com>
2024-09-28 20:48:27 +0330

    Click Events

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const signUpBtn = document.querySelector("#signUp");
 const fistForm = document.querySelector("#form1");
 const secondForm = document.querySelector("#form2");
 const container = document.querySelector(".container");
+
+
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
