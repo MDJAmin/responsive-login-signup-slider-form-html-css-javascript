ae97348ef3c98c5937a9caadd83500f75caa4dbb
MDJAmin <mohammadamindf@gmail.com>
2024-09-28 20:47:47 +0330

    Get elements from dom

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const signInBtn = document.querySelector("#signIn");
+const signUpBtn = document.querySelector("#signUp");
+const fistForm = document.querySelector("#form1");
+const secondForm = document.querySelector("#form2");
+const container = document.querySelector(".container");
